130dd7f1e3422a4d03d49afa1c465ffba0e9aef8
Hoang Giang Bui <hgbk2008@gmail.com>
2018-02-27 08:12:28 +0100

    modify to compile with boost_1_64_0

--- kratos/python/add_matrix_market_interface_to_python.cpp
+++ kratos/python/add_matrix_market_interface_to_python.cpp
@@ -29,8 +29,8 @@
 
 // Project includes
 #include "includes/define.h"
-#include "includes/matrix_market_interface.h"
 #include "includes/ublas_interface.h"
+#include "includes/matrix_market_interface.h"
 #include "python/add_matrix_market_interface_to_python.h"
 
 namespace Kratos
