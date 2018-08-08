d6c54cc918522ccaa5d719d708f3cfe1f91e2a8a
vryy <hgbk2008@gmail.com>
2018-08-08 15:54:43 +0200

    improve the NoMpi

--- external_libraries/mpi_python/nompi.py
+++ external_libraries/mpi_python/nompi.py
@@ -1,7 +1,18 @@
+
+
 class Mpi:
+
+    class World:
+        def __init__(self):
+            pass
+
+        def barrier(self):
+            pass
+
     def __init__(self):
         self.size = 1
         self.rank = 0
+        self.world = Mpi.World()
 
 mpi = Mpi()
 
