26e131cc43f5e93f8627d6c3138f57f44f444d39
vryy <hgbk2008@gmail.com>
2019-04-23 17:07:11 +0200

    add the utilities to create the linear constraint

--- kratos/utilities/constraint_utilities.h
+++ kratos/utilities/constraint_utilities.h
@@ -49,6 +49,7 @@ namespace Kratos
  */
 class ConstraintUtilities
 {
+public:
     /**
      * @brief This method resets the values of the slave dofs
      * @param rModelPart The model of the problem to solve
@@ -89,6 +90,38 @@ class ConstraintUtilities
         const std::string DofRotationVariableName = "ROTATION",
         const std::string InertiaVariableName = "NODAL_INERTIA_TENSOR"
         );
+
+    /**
+     * Add a complex linear constraint to the model_part. The constraint is of type
+     *   u1 = a*u2 + b*u3 + c*u4 + ...
+     * @param rModelPart the model_part to add the constraint
+     * @param pSlaveNode the slave node carrying the slave d.o.f
+     * @param pMasterNodes the master nodes carrying the master d.o.fs
+     * @param rVariable the variable representing the nodal d.o.f
+     * @param r_relation_matrix the relation matrix of size(1, number_of_master_nodes)
+     * @param r_constant_vector the constant vector of size 1
+     */
+    template<typename TNodePointerType, typename TVariableType, typename TMatrixType, typename TVectorType>
+    static void CreateLinearConstraint(ModelPart& rModelPart,
+        const std::string& r_constraint_name,
+        const std::size_t& r_constraint_id,
+        TNodePointerType pSlaveNode,
+        std::vector<TNodePointerType> pMasterNodes,
+        const TVariableType& rVariable,
+        const TMatrixType& r_relation_matrix,
+        const TVectorType& r_constant_vector)
+    {
+        typedef typename TNodePointerType::element_type::DofType DofType;
+
+        std::vector<typename DofType::Pointer> slave_dofs(1);
+        slave_dofs[0] = pSlaveNode->pGetDof(rVariable);
+
+        std::vector<typename DofType::Pointer> master_dofs(pMasterNodes.size());
+        for (std::size_t i = 0; i < pMasterNodes.size(); ++i)
+            master_dofs[i] = pMasterNodes[i]->pGetDof(rVariable);
+
+        rModelPart.CreateNewMasterSlaveConstraint(r_constraint_name, r_constraint_id, master_dofs, slave_dofs, r_relation_matrix, r_constant_vector);
+    }
 
 }; // namespace ConstraintUtilities
 }  // namespace Kratos
