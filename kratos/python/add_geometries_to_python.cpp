57b20439b86b098c0ee07a9021e8d939eb242477
vryy <hgbk2008@gmail.com>
2019-12-20 19:10:59 +0100

    enhance the Python interface for Geometry

--- kratos/python/add_geometries_to_python.cpp
+++ kratos/python/add_geometries_to_python.cpp
@@ -1,10 +1,10 @@
-//    |  /           |             
+//    |  /           |
 //    ' /   __| _` | __|  _ \   __|
 //    . \  |   (   | |   (   |\__ `
 //   _|\_\_|  \__,_|\__|\___/ ____/
-//                   Multi-Physics 
+//                   Multi-Physics
 //
-//  License:		 BSD License 
+//  License:		 BSD License
 //					 Kratos default license: kratos/license.txt
 //
 //  Main authors:    Riccardo Rossi
@@ -32,27 +32,47 @@ namespace Kratos
 
 namespace Python
 {
-    const PointerVector< Node<3> >& ConstGetPoints( Geometry<Node<3> >& geom ) { return geom.Points(); }
-    PointerVector< Node<3> >& GetPoints( Geometry<Node<3> >& geom ) { return geom.Points(); }
-    
+
+const PointerVector< Node<3> >& ConstGetPoints( Geometry<Node<3> >& geom ) { return geom.Points(); }
+PointerVector< Node<3> >& GetPoints( Geometry<Node<3> >& geom ) { return geom.Points(); }
+
+bool Geometry_IsInside1(Geometry<Node<3> >& rDummy, boost::python::list& point)
+{
+    Geometry<Node<3> >::CoordinatesArrayType Point;
+    Point[0] = boost::python::extract<double>(point[0]);
+    Point[1] = boost::python::extract<double>(point[1]);
+    Point[2] = boost::python::extract<double>(point[2]);
+
+    Geometry<Node<3> >::CoordinatesArrayType Result;
+    return rDummy.IsInside(Point, Result);
+}
+
+bool Geometry_IsInside2(Geometry<Node<3> >& rDummy, Geometry<Node<3> >::CoordinatesArrayType& rPoint)
+{
+    Geometry<Node<3> >::CoordinatesArrayType Result;
+    return rDummy.IsInside(rPoint, Result);
+}
+
 void  AddGeometriesToPython()
 {
-  
+
     typedef Geometry<Node<3> > GeometryType;
     class_<GeometryType, GeometryType::Pointer >("Geometry", init<>())
     .def(init< GeometryType::PointsArrayType& >())
 //     .def("Points", &GeometryType::ConstGetPoints)
 //     .def("Points", &GeometryType::GetPoints)
+    .def("IsInside", &Geometry_IsInside1)
+    .def("IsInside", &Geometry_IsInside2)
     ;
-    
+
     class_<Triangle2D3<Node<3> >, Triangle2D3<Node<3> >::Pointer, bases< GeometryType > >("Triangle2D3", init<Node<3>::Pointer, Node<3>::Pointer, Node<3>::Pointer>())
-    ;    
-    
-    
+    ;
+
+
 //     class_<GeometryType, GeometryType::Pointer, bases<PointerVector< Node<3> > > >("Geometry", init<>())
 //      .def(init< GeometryType::PointsArrayType& >())
 //      ;
-     
+
 }
 
 }  // namespace Python.
