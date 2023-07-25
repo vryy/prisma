9fbd31dc1ab0c9dcf07a2e4244b5949f85065a72
Hoang-Giang Bui <hgbk2008@gmail.com>
2023-07-25 14:13:07 +0200

    Clean up

--- kratos/sources/legacy_structural_app_vars.cpp
+++ kratos/sources/legacy_structural_app_vars.cpp
@@ -1,25 +1,14 @@
-//    |  /           | 
-//    ' /   __| _` | __|  _ \   __| 
+//    |  /           |
+//    ' /   __| _` | __|  _ \   __|
 //    . \  |   (   | |   (   |\__ \.
-//   _|\_\_|  \__,_|\__|\___/ ____/ 
-//                   Multi-Physics  
+//   _|\_\_|  \__,_|\__|\___/ ____/
+//                   Multi-Physics
 //
-//  License:		 BSD License 
-//					 Kratos default license: kratos/license.txt
+//  License:         BSD License
+//                   Kratos default license: kratos/license.txt
 //
 //  Main authors:    Riccardo Rossi
 //
-
-
-
-
-
-
-
-
-
-
-
 
 // System includes
 #include <string>
