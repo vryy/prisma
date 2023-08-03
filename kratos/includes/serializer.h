093ee388c05bba069fcfef769c164e8cb113d152
Hoang-Giang Bui <hgbk2008@gmail.com>
2023-08-03 23:12:53 +0200

    Clean up Serializer, wrap by KRATOS_PYTHON the Python related part

--- kratos/includes/serializer.h
+++ kratos/includes/serializer.h
@@ -5,12 +5,12 @@
 //
 // Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 //
-// 	-	Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-// 	-	Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer
-// 		in the documentation and/or other materials provided with the distribution.
-// 	-	All advertising materials mentioning features or use of this software must display the following acknowledgement:
-// 			This product includes Kratos Multi-Physics technology.
-// 	-	Neither the name of the CIMNE nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+//  -   Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//  -   Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer
+//      in the documentation and/or other materials provided with the distribution.
+//  -   All advertising materials mentioning features or use of this software must display the following acknowledgement:
+//          This product includes Kratos Multi-Physics technology.
+//  -   Neither the name of the CIMNE nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 //
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ''AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
 // THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
@@ -37,8 +37,9 @@
 
 
 // External includes
+#ifdef KRATOS_PYTHON
 #include <boost/python/object.hpp>
-
+#endif
 
 
 // Project includes
@@ -197,8 +198,8 @@ public:
     {
         msRegisteredObjects.insert(RegisteredObjectsContainerType::value_type(rName,Create<TDataType>));
         msRegisteredObjectsName.insert(RegisteredObjectsNameContainerType::value_type(typeid(TDataType).name(), rName));
-	//std::cout<<" REGISTERED OBJECT "<<rName<<" TypeID "<<typeid (TDataType).name()<<std::endl;
-	//msRegisteredObjects.insert(RegisteredObjectsContainerType::value_type(rName,&pPrototype));
+        //std::cout<<" REGISTERED OBJECT "<<rName<<" TypeID "<<typeid (TDataType).name()<<std::endl;
+        //msRegisteredObjects.insert(RegisteredObjectsContainerType::value_type(rName,&pPrototype));
     }
 
     template<class TDataType>
@@ -354,16 +355,18 @@ public:
     void load(std::string const & rTag, array_1d<TDataType, TDimension>& rObject)
     {
         load_trace_point(rTag);
-	//rObject = array_1d<TDataType, TDimension>(); //it generates a warnning --> commented 23/09/2015 <--
+        //rObject = array_1d<TDataType, TDimension>(); //it generates a warnning --> commented 23/09/2015 <--
         for(SizeType i = 0 ; i < TDimension ; i++)
             load("E", rObject[i]);
 //    read(rObject);
     }
 
+    #ifdef KRATOS_PYTHON
     void load(std::string const & rTag, boost::python::object& rObject)
     {
         // hbui: TODO
     }
+    #endif
 
     KRATOS_SERIALIZATION_DIRECT_LOAD(bool)
     KRATOS_SERIALIZATION_DIRECT_LOAD(int)
@@ -459,19 +462,21 @@ public:
         }
     }
 
+    #ifdef KRATOS_PYTHON
     void save(std::string const & rTag, boost::python::object const& rObject)
     {
         // hbui: TODO
     }
+    #endif
 
     template<class TDataType>
     bool IsDerived(TDataType * pValue)
     {
       if (strcmp(typeid(TDataType).name(), typeid(*pValue).name()) != 0) {
-	return true;
+        return true;
       }
       else {
-	return false;
+        return false;
       }
       // bool is_derived = (typeid(TDataType) != typeid(*pValue));
 //    std::cout << "for TDataType : " << typeid(TDataType).name() << " and *pValue type : " << typeid(*pValue).name() << " is derived : " << is_derived << std::endl;
