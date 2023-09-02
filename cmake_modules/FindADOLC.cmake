9434c2c45809e3ebd12451a68326a95f6cf8065c
Hoang-Giang Bui <hgbk2008@gmail.com>
2023-09-02 22:31:04 +0200

    Improve cmake files

--- cmake_modules/FindADOLC.cmake
+++ cmake_modules/FindADOLC.cmake
@@ -4,19 +4,18 @@
 
 FIND_LIBRARY(ADOL_C_LIBRARY adolc PATHS "${ADOL_C_DIR}/lib64" NO_DEFAULT_PATH)
 
-message("ADOL_C_DIR:" ${ADOL_C_DIR})
-message("ADOL_C_LIBRARY:" ${ADOL_C_LIBRARY})
+# message("ADOL_C_DIR:" ${ADOL_C_DIR})
+# message("ADOL_C_LIBRARY:" ${ADOL_C_LIBRARY})
 
 IF(ADOL_C_LIBRARY)
     SET(ADOL_C_INCLUDE_DIR "${ADOL_C_DIR}/include")
     IF(ADOL_C_LIBRARY)
         SET(ADOL_C_LIBRARIES ${ADOL_C_LIBRARY})
         SET(ADOL_C_FOUND TRUE)
-        message("ADOL-C found at" ${ADOL_C_DIR})
+        message("-- Found ADOL-C at " ${ADOL_C_DIR})
     ELSE()
-        message("finding ADOL-C failed, please try to set the var ADOL_C_DIR")
+        message("-- Finding ADOL-C failed, please try to set the var ADOL_C_DIR")
     ENDIF()
 ELSE()
-    message("finding ADOL-C failed, please try to set the var ADOL_C_DIR")
+    message("-- Finding ADOL-C failed, please try to set the var ADOL_C_DIR")
 ENDIF()
-
