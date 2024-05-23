569eb15c36efe124b040cb3dc76119ec377fcb63
Hoang-Giang Bui <hgbk2008@gmail.com>
2024-05-23 11:11:58 +0200

    Fix and improve Gauss-Legendre quadrature on triangle

--- kratos/integration/triangle_gauss_legendre_integration_points.h
+++ kratos/integration/triangle_gauss_legendre_integration_points.h
@@ -143,15 +143,19 @@ public:
 
     typedef IntegrationPointType::PointType PointType;
 
-    static constexpr SizeType IntegrationPointsNumber()    {  return 6; }
+    static constexpr SizeType IntegrationPointsNumber()
+    {
+        return 6;
+    }
 
     static const IntegrationPointsArrayType& IntegrationPoints()
     {
         // Reference:
         // +    https://people.sc.fsu.edu/~jburkardt/datasets/quadrature_rules_tri/strang5_x.txt
         // +    https://people.sc.fsu.edu/~jburkardt/datasets/quadrature_rules_tri/strang5_w.txt
-        constexpr double wa = 0.109951743655322;
-        constexpr double wb = 0.223381589678011;
+        // (Note: divide weight by half)
+        constexpr double wa = 0.054975871827661;
+        constexpr double wb = 0.1116907948390055;
         constexpr double Na1 = 0.816847572980459;
         constexpr double Nb1 = 0.108103018168070;
         constexpr double Na2 = 0.091576213509771;
@@ -177,6 +181,63 @@ public:
 }; // Class TriangleGaussLegendreIntegrationPoints4
 
 
+class KRATOS_API(KRATOS_CORE) TriangleGaussLegendreIntegrationPoints5
+{
+public:
+    typedef std::size_t SizeType;
+
+    static const unsigned int Dimension = 2;
+
+    typedef IntegrationPoint<2> IntegrationPointType;
+
+    typedef boost::array<IntegrationPointType, 12> IntegrationPointsArrayType;
+
+    typedef IntegrationPointType::PointType PointType;
+
+    static SizeType IntegrationPointsNumber()
+    {
+        return 12;
+    }
+
+    static const IntegrationPointsArrayType& IntegrationPoints()
+    {
+        constexpr double wa = 0.025422453185103408460;
+        constexpr double wb = 0.058393137863189683013;
+        constexpr double wc = 0.041425537809186787597;
+
+        constexpr double N1 = 0.87382197101699554332;
+        constexpr double N2 = 0.063089014491502228340;
+        constexpr double N3 = 0.50142650965817915742;
+        constexpr double N4 = 0.24928674517091042129;
+        constexpr double N5 = 0.053145049844816947353;
+        constexpr double N6 = 0.31035245103378440542;
+        constexpr double N7 = 0.63650249912139864723;
+
+        static const IntegrationPointsArrayType s_integration_points{{
+            IntegrationPointType( N1, N2, wa ),
+            IntegrationPointType( N2, N1, wa ),
+            IntegrationPointType( N2, N2, wa ),
+            IntegrationPointType( N3, N4, wb ),
+            IntegrationPointType( N4, N3, wb ),
+            IntegrationPointType( N4, N4, wb ),
+            IntegrationPointType( N5, N6, wc ),
+            IntegrationPointType( N6, N5, wc ),
+            IntegrationPointType( N5, N7, wc ),
+            IntegrationPointType( N6, N7, wc ),
+            IntegrationPointType( N7, N5, wc ),
+            IntegrationPointType( N7, N6, wc )
+        }};
+        return s_integration_points;
+    }
+
+    std::string Info() const
+    {
+        std::stringstream buffer;
+        buffer << "Triangle Gauss-Legendre quadrature 5 ";
+        return buffer.str();
+    }
+}; // Class TriangleGaussLegendreIntegrationPoints5
+
 ///@name Type Definitions
 ///@{
 
@@ -191,4 +252,4 @@ public:
 
 }  // namespace Kratos.
 
-#endif // KRATOS_TRIANGLE_GAUSS_LEGENDRE_INTEGRATION_POINTS_H_INCLUDED  defined 
+#endif // KRATOS_TRIANGLE_GAUSS_LEGENDRE_INTEGRATION_POINTS_H_INCLUDED  defined
