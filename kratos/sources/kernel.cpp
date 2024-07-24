4892c79b566f132e18d15033a202dd1da821a525
Hoang-Giang Bui <hgbk2008@gmail.com>
2024-07-24 17:09:42 +0200

    Refactoring

--- kratos/sources/kernel.cpp
+++ kratos/sources/kernel.cpp
@@ -10,35 +10,22 @@
 //  Main authors:    Pooyan Dadvand
 //
 
-
-
-
-
-
-
-
-
-
-
 #include "includes/kernel.h"
 #include "includes/kratos_version.h"
-
 
 namespace Kratos
 {
     Kernel::Kernel()
     {
-        std::cout << "MAINTAINED" << std::endl;
         std::cout << " |  /           |" << std::endl;
         std::cout << " ' /   __| _` | __|  _ \\   __|" << std::endl;
         std::cout << " . \\  |   (   | |   (   |\\__ \\" << std::endl;
         std::cout << "_|\\_\\_|  \\__,_|\\__|\\___/ ____/" << std::endl;
-        // std::cout << "           Multi-Physics "<< KRATOS_VERSION << std::endl;
-        std::cout << "           Multi-Physics" << std::endl;
-        std::cout << "             for Python 2" << std::endl;
-        std::cout << "   by Hoang-Giang Bui" << std::endl;
-        std::cout << "     Ruhr University Bochum, 2013-2021" << std::endl;
-        std::cout << "     Helmholtz-Zentrum Hereon, 2022-2023" << std::endl;
+        std::cout << "         Multi-Physics (Python 2 version)" << std::endl;
+        std::cout << "   maintained by Hoang-Giang Bui" << std::endl;
+        std::cout << "     Ruhr University Bochum     2013-2021" << std::endl;
+        std::cout << "     Helmholtz-Zentrum Hereon   2022-2024" << std::endl;
+        std::cout << "     University of Birmingham   2025-2026" << std::endl;
 
         mKratosApplication.RegisterVariables();
     }
@@ -48,61 +35,16 @@ namespace Kratos
         unsigned int j = 0;
         for(KratosComponents<VariableData>::ComponentsContainerType::iterator i = KratosComponents<VariableData>::GetComponents().begin() ;
                 i != KratosComponents<VariableData>::GetComponents().end() ; i++)
-            //const_cast<VariableData&>(i->second.get()).SetKey(++j);
             i->second->SetKey(++j);
     }
 
     void Kernel::AddApplication(KratosApplication& NewApplication)
     {
-        //typedef VariableComponent<VectorComponentAdaptor<array_1d<double, 3> > > array_1d_component_type;
-
         NewApplication.Register();
-
-        /*
-        KratosComponents<VariableData>::GetComponents().insert(NewApplication.GetVariables().begin(),NewApplication.GetVariables().end());
-
-        KratosComponents<Variable<int> >::GetComponents().insert(NewApplication.GetComponents(Variable<int>("NONE")).begin(),
-                NewApplication.GetComponents(Variable<int>("NONE")).end());
-        KratosComponents<Variable<unsigned int> >::GetComponents().insert(NewApplication.GetComponents(Variable<unsigned int>("NONE")).begin(),
-               NewApplication.GetComponents(Variable<unsigned int>("NONE")).end());
-        KratosComponents<Variable<double> >::GetComponents().insert(NewApplication.GetComponents(Variable<double>("NONE")).begin(),
-                NewApplication.GetComponents(Variable<double>("NONE")).end());
-        KratosComponents<Variable<array_1d<double, 3> > >::GetComponents().insert(NewApplication.GetComponents(Variable<array_1d<double, 3> >("NONE")).begin(),
-                NewApplication.GetComponents(Variable<array_1d<double, 3> >("NONE")).end());
-        KratosComponents<Variable<Vector> >::GetComponents().insert(NewApplication.GetComponents(Variable<Vector>("NONE")).begin(),
-                NewApplication.GetComponents(Variable<Vector>("NONE")).end());
-        KratosComponents<Variable<Matrix> >::GetComponents().insert(NewApplication.GetComponents(Variable<Matrix>("NONE")).begin(),
-                NewApplication.GetComponents(Variable<Matrix>("NONE")).end());
-
-        Kratos::VectorComponentAdaptor<Kratos::array_1d<double, 3> > temp_adaptor(DISPLACEMENT, 0); // the displacement is not important, only an array_1d variable is needed!
-
-        KratosComponents<array_1d_component_type>::GetComponents().insert(NewApplication.GetComponents(array_1d_component_type("NONE", temp_adaptor)).begin(),
-                NewApplication.GetComponents(array_1d_component_type("NONE", temp_adaptor)).end());
-
-        KratosComponents<Element>::GetComponents().insert(NewApplication.GetElements().begin(),
-                NewApplication.GetElements().end());
-        KratosComponents<Condition>::GetComponents().insert(NewApplication.GetConditions().begin(),
-                NewApplication.GetConditions().end());
-
-//        KratosComponents<Variable<double> >::GetComponents().insert(NewApplication.GetComponents(Variable<double>("NONE")).begin(),
-//                NewApplication.GetComponents(Variable<double>("NONE")).end());
-
-
-        Serializer::GetRegisteredObjects().insert(NewApplication.GetRegisteredObjects().begin(), NewApplication.GetRegisteredObjects().end());
-        Serializer::GetRegisteredObjectsName().insert(NewApplication.GetRegisteredObjectsName().begin(), NewApplication.GetRegisteredObjectsName().end());
-        */
     }
 
     void Kernel::InitializeApplication(KratosApplication& NewApplication)
     {
-        /*
-        NewApplication.SetComponents(KratosComponents<VariableData>::GetComponents());
-        NewApplication.SetComponents(KratosComponents<Element>::GetComponents());
-        NewApplication.SetComponents(KratosComponents<Condition>::GetComponents());
-
-        NewApplication.GetRegisteredObjects().insert(Serializer::GetRegisteredObjects().begin(), Serializer::GetRegisteredObjects().end());
-        NewApplication.GetRegisteredObjectsName().insert(Serializer::GetRegisteredObjectsName().begin(), Serializer::GetRegisteredObjectsName().end());
-        */
     }
 
     std::string Kernel::Info() const
@@ -128,6 +70,3 @@ namespace Kratos
         KratosComponents<Condition>().PrintData(rOStream);
     }
 }
-
-
-
