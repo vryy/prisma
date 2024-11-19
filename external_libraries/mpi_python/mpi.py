fa28399b9562e12c985ac6cc1e9198e3d366e6a3
Hoang-Giang Bui <hgbk2008@gmail.com>
2024-11-19 20:15:44 +0100

    Adjust for Python 3

--- external_libraries/mpi_python/mpi.py
+++ external_libraries/mpi_python/mpi.py
@@ -1,9 +1,14 @@
 from __future__ import print_function, absolute_import, division #makes KratosMultiphysics backward compatible with python 2.6 and 2.7
 import sys
 if sys.platform.startswith('linux'):
-    import DLFCN as dl
+    try:
+        import DLFCN as dl
+        mode = dl.RTLD_NOW | dl.RTLD_GLOBAL
+    except ImportError:  # For Python 3
+        import os
+        mode = os.RTLD_NOW | os.RTLD_GLOBAL
     flags = sys.getdlopenflags()
-    sys.setdlopenflags(dl.RTLD_NOW | dl.RTLD_GLOBAL)
+    sys.setdlopenflags(mode)
     import mpipython
     sys.setdlopenflags(flags)
 else:
