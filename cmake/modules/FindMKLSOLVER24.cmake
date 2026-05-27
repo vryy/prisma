e1f872241622581f4cca5fdcff6b13cc46c970f9
Hoang-Giang Bui <hgbk2008@gmail.com>
2026-05-27 18:09:02 +0100

    Using target for the cmake module of MKL

--- cmake/modules/FindMKLSOLVER24.cmake
+++ cmake/modules/FindMKLSOLVER24.cmake
@@ -1,73 +1,66 @@
-# Find the PARDISO that comes with MKL includes and libraries
+# Find the MKL includes and libraries which includes high performance (and free) PARDISO solver
 #
+# It creates the target MKLSolverLibraries which can be linked against using
+#   target_link_libraries(KratosMKLSolversApplication PUBLIC MKLSolverLibraries)
+# but the configure script must provide
+#   MKLSOLVER_INCLUDE_DIR - where to find the headers
+#   MKLSOLVER_LIB_DIR     - where to find the libraries
 
-# MKLSOLVER_INCLUDE_DIR - where to find autopack.h
-# MKLSOLVER_LIBRARIES   - List of fully qualified libraries to link against.
-# MKLSOLVER_FOUND       - Do not attempt to use if "no" or undefined.
-
-FIND_PATH(MKLSOLVER_INCLUDE_DIR mkl_blas.h
-  ${MKLSOLVER_INCLUDE_DIR}
-)
+find_path(MKLSOLVER_INCLUDE_DIR mkl_blas.h ${MKLSOLVER_INCLUDE_DIR})
 
 # set(LIB_MKL_LIST mkl_intel_ilp64 mkl_intel_thread mkl_core mkl_solver_ilp64 mkl_mc mkl_mc3 mkl_lapack pthread mkl_p4n iomp5)
 
-set(MY_LIB_SEARCH_DIR ${MKLSOLVER_LIB_DIR})
-
-FIND_LIBRARY(AUX1 mkl_intel_ilp64
-  ${MY_LIB_SEARCH_DIR}
-)
+find_library(AUX1 mkl_intel_ilp64 ${MKLSOLVER_LIB_DIR})
 
 if(CMAKE_COMPILER_IS_GNUCXX)
-    FIND_LIBRARY(AUX2 mkl_gnu_thread
-      ${MY_LIB_SEARCH_DIR}
-    )
+    find_library(AUX2 mkl_gnu_thread ${MKLSOLVER_LIB_DIR})
 else()
-    FIND_LIBRARY(AUX2 mkl_intel_thread
-      ${MY_LIB_SEARCH_DIR}
-    )
-endif( )
+    find_library(AUX2 mkl_intel_thread ${MKLSOLVER_LIB_DIR})
+endif()
 
-FIND_LIBRARY(AUX3 mkl_core
-  ${MY_LIB_SEARCH_DIR}
-)
+find_library(AUX3 mkl_core ${MKLSOLVER_LIB_DIR})
 
-#FIND_LIBRARY(AUX5 mkl_mc # kernel library for SSE3
-#  ${MY_LIB_SEARCH_DIR}
+#find_library(AUX5 mkl_mc # kernel library for SSE3
+#  ${MKLSOLVER_LIB_DIR}
 #)
 
 if(NOT(${CMAKE_SYSTEM_NAME} MATCHES "Windows"))
-  FIND_LIBRARY(AUX6 mkl_mc3 # kernel library for SSE4.2
-    ${MY_LIB_SEARCH_DIR}
-  )
+  find_library(AUX6 mkl_mc3 ${MKLSOLVER_LIB_DIR}) # kernel library for SSE4.2
 endif()
 
-#FIND_LIBRARY(AUX7 mkl_lapack95_ilp64
-#  ${MY_LIB_SEARCH_DIR}
-#)
+#find_library(AUX7 mkl_lapack95_ilp64 ${MKLSOLVER_LIB_DIR})
 
 if(NOT(${CMAKE_SYSTEM_NAME} MATCHES "Windows"))
-  FIND_LIBRARY(AUX8 pthread
-    ${MY_LIB_SEARCH_DIR}
-  )
+  find_library(AUX8 pthread ${MKLSOLVER_LIB_DIR})
 endif()
 
-#FIND_LIBRARY(AUX9 mkl_p4n
-#  ${MY_LIB_SEARCH_DIR}
-#)
+#find_library(AUX9 mkl_p4n ${MKLSOLVER_LIB_DIR})
 
 ##ATTENTION! the order of libraries is FUNDAMENTAL!! take care!!!
-IF(MKLSOLVER_INCLUDE_DIR)
-  IF(AUX1)
-#    SET( MKLSOLVER_LIBRARIES ${AUX9} ${AUX8} ${AUX6} ${AUX5}  ${AUX7} ${AUX4} ${AUX3} ${AUX2}  ${AUX1}  )
-     SET( MKLSOLVER_LIBRARIES ${AUX1} ${AUX2} ${AUX3} ${AUX4}  ${AUX7} ${AUX5} ${AUX6} ${AUX8}  ${AUX9}  )
-    SET( MKLSOLVER_FOUND "YES" )
-    include_directories( ${MKLSOLVER_INCLUDE_DIR} )
-    message("************ mkl solver libraries found  ${MKLSOLVER_LIBRARIES}")
-  ELSE(AUX1)
-    message("finding MKLSOLVER library failed, please try to set the var MKLSOLVER_ROOT_DIR")
-  ENDIF(AUX1)
-ELSE(MKLSOLVER_INCLUDE_DIR)
-  message("finding MKLSOLVER includes failed, please try to set the var MKLSOLVER_ROOT_DIR")
-ENDIF(MKLSOLVER_INCLUDE_DIR)
+if(MKLSOLVER_INCLUDE_DIR AND AUX1)
+    set( MKLSOLVER_FOUND "YES" )
 
+    if(NOT TARGET MKLSOLVER::lib)
 
+        add_library(MKLSOLVER::lib INTERFACE IMPORTED GLOBAL)
+
+        target_compile_definitions(MKLSOLVER::lib INTERFACE MKL_ILP64)
+
+        target_link_libraries(MKLSOLVER::lib INTERFACE
+            ${AUX1} ${AUX2} ${AUX3} ${AUX4}  ${AUX7} ${AUX5} ${AUX6} ${AUX8}  ${AUX9}
+        )
+
+        target_include_directories(MKLSOLVER::lib INTERFACE ${MKLSOLVER_INCLUDE_DIR})
+
+        set_target_properties(MKLSOLVER::lib PROPERTIES INTERFACE_LINK_DIRECTORIES "${MKLSOLVER_LIB_DIR}")
+
+        if(CMAKE_INSTALL_RPATH_USE_LINK_PATH)
+          target_link_options(MKLSOLVER::lib INTERFACE "-Wl,-rpath,${MKLSOLVER_LIB_DIR}")
+        endif()
+
+        message("************ mkl solver libraries found ************")
+
+    endif()
+else()
+    message("finding MKLSOLVER includes failed. Try to set MKLSOLVER_INCLUDE_DIR and MKLSOLVER_LIB_DIR.")
+endif()
